916af818b313cf9bb8ac53071f0d93f28dc48693
Harris Novick <harris@harrisnovick.com>
2013-02-11 03:26:21 -0500

    rails_apps_composer generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Totemtext::Application.config.secret_token = 'a476c7319f839e2583ba702e3b594d6a27f10f950ed8e9ed320c383a2014b522536404defd16959c5819010704d3c7edf90075c5bca64cfb2f12e7260b2fde13'
